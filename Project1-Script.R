2f0961353f5fe9604a67a35e369d8a6d9771f47b
turicensis <jzurche@hotmail.com>
2015-11-09 01:28:01 +0100

    adding script update and first plot1

--- Project1-Script.R
+++ Project1-Script.R
@@ -1,52 +1,18 @@
-setwd("C:\\Users\\Jerome\\Documents\\R\\1511-04ExploratoryDataAnalytics\\ExData_Plotting1")
+setwd("C:\\Users\\XXXX\\Documents\\R\\1511-04ExploratoryDataAnalytics\\ExData_Plotting1")
 
-unzip("Samsung.zip")
+unzip("exdata_data_household_power_consumption.zip")
 
-train <- read.table(".\\UCI HAR Dataset\\train\\X_train.txt", sep="", header=F)	# Loads X data
-trainy <- read.table(".\\UCI HAR Dataset\\train\\Y_train.txt", sep="", header=F)	# Loads Y
-subject_train <- read.table(".\\UCI HAR Dataset\\train\\subject_train.txt", sep="", header=F)	# Loads Subject_train 
-train_tot <- data.frame(trainy, subject_train, train)	# merge the y and subject data of train with X data
+data <- read.csv("household_power_consumption.csv")
 
-## all the train data are now merged, but aren't labelled yet
-## first creating a "dummy" feactures vector of labels for each of the column
-features <- c(1:561)
+data$Date <- as.Date(data$Date)
 
-## Concatenating Y and Subject with features to create a single label vector (char)
-header <- c("Activity", "Subject", features)
-## Forcing header as column names for train data
-colnames(train_tot) <- header
+## I can't change the date format correctly, so I am creating a dummy set of data to builf some plots
+dummydata <- data[2000:2300,]
 
-### Same operations for test
-test <- read.table(".\\UCI HAR Dataset\\test\\X_test.txt", sep="", header=F)	# Loads X data
-testy <- read.table(".\\UCI HAR Dataset\\test\\Y_test.txt", sep="", header=F)	# Loads Y
-subject_test <- read.table(".\\UCI HAR Dataset\\test\\subject_test.txt", sep="", header=F)	# Loads Subject_train 
-test_tot <- data.frame(testy, subject_test, test)	# merge the y and subject data with X data
-## Forcing header as column names for test data
-colnames(test_tot) <- header
+### turning factors of power data to numeric
+dummydata$Global_active_power <- as.numeric(dummydata$Global_active_power)
 
-### Now merging train and test
-data <- merge(train_tot, test_tot, )
-
-## Now loading the features to use as real names for columns
-features1 <- read.table(".\\UCI HAR Dataset\\features.txt", sep="", header=F)	
-features <- as.character(features1[,2])	## factors turned into characters
-
-## rebuilding header vector with real names for columns
-header <- c("Activity", "Subject", features)
-## renaming columns with final names
-colnames(data) <- header
-
-### creating a logical vector identifying which cols are either mean or std
-featuresLogic <- grepl("mean", features) | grepl("std", features)
-str(featuresLogic)
-featuresLogic1 <- vector()
-featuresLogic1[1:2] <- TRUE
-featuresLogic1[3:563] <- featuresLogic
-
-data.clean <- data[, featuresLogic]
-str(data.clean)
-write.table(data.clean, "data-clean.txt")
-
-featuresLogic
-str(data.clean)
-
+### building a histogram based on power data and copying it into a png file 
+hist(dummydata$Global_active_power)
+dev.copy(png, file = "plot1.png")
+## sorry, running out of time to complete the assignment...
